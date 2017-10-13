f8a0199cdaee3d946e1079eb3990953d96b1404b
Jan Sebastian Vigar <jvigar@jans-mbp.attlocal.net>
2017-10-12 22:44:04 -0700

    feat: add DeckList and Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
+
+const Deck = ({ title, cardsCount }) => (
+  <View style={styles.container}>
+    <Text>{title}</Text>
+    <Text>{cardsCount} cards</Text>
+  </View>
+);
 
 Deck.propTypes = {
   title: PropTypes.string.isRequired,
+  cardsCount: PropTypes.number.isRequired,
 };
 
-const Deck = props => (
-  <View>
-    <Text>{props.title}</Text>
-  </View>
-);
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 0.5,
+    borderColor: '#8ACFFF',
+    borderRadius: 4,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Deck;
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import Deck from './Deck';
 
 const DeckList = () => (
-  <View>
-    <Text>DeckList</Text>
-  </View>
+  <ScrollView contentContainerStyle={styles.container}>
+    <Deck title="Deck 1" cardsCount={4} />
+    <Deck title="Deck 2" cardsCount={5} />
+    <Deck title="Deck 3" cardsCount={1} />
+    <Deck title="Deck 4" cardsCount={3} />
+  </ScrollView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 50,
+    marginRight: 50,
+  },
+});
 
 export default DeckList;
